28eeca1e7818494cfa4dacf5544b2ab501609e0c
Raul Romero <raul.romero.cmt@gmail.com>
2022-02-17 22:45:39 -0600

    vue start

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Cubreasientos',
+            design_title: 'Diseña tu Modelo',
+            main_bg_image: './assets/images/2022-covercar-logo.png',
+            custom_made_patterns_icon: './assets/textile-industry-icons/patterns.png',
+            cs_email: 'atencion@covercar.mx'
+        }
+    }
+})
