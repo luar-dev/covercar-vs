63d7a26c8f6cda71438baf472317c20767480252
Raul Romero <raul.romero.cmt@gmail.com>
2022-02-20 14:11:37 -0600

    palette

--- main.js
+++ main.js
@@ -10,11 +10,27 @@ const app = Vue.createApp({
             seat_black_gray: false,
             seat_gray_black: false,
             seat_black_beige: false,
+
+            imagePath: './assets/images/seat_blank.png',
+
+            seatColorCombinations: [
+                //{id: 0, class: '', imagePath: './assets/images/seat_blank.png'},
+                {id: 1, class: 'seat_combination black_gray', imagePath: './assets/images/seat-black_gray.png'},
+                {id: 2, class: 'seat_combination gray_black', imagePath: './assets/images/seat-gray_black.png'},
+                {id: 3, class: 'seat_combination black_beige', imagePath: './assets/images/seat-black_beige.png'},
+            ],
+
             materials: [
                 {id: 4567, name: 'Tela Automotriz'},
                 {id: 4950, name: 'Vinipiel'},
                 {id: 4943, name: 'Tela con Vinipiel'}
             ],
         }
+    },
+
+    methods: {
+        setColorCombination(variantImage) {
+            this.imagePath = variantImage;
+        },
     }
 })
